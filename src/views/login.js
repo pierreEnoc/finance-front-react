1f54d55fb2f856e7eb7394ddf5d8259e0d481c89
pierreEnoc <pierre_enoc@yahoo.fr>
2020-05-31 12:20:03 -0300

    start login

---
+++ src/views/login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Card from '../components/card'
+
+class Login extends React.Component {
+
+    render(){
+        return(
+
+            <div className="container">
+                <div className ="row"> 
+                  <div className ="col-md-6" style={{position : 'relative'}}>
+                      <div className ="bs-docs-section">
+                      <Card title="Login">
+                          Meu catao
+
+                        </Card>
+
+                      </div>
+                      
+
+                  </div>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Login
