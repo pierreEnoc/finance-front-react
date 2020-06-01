9d5dbbc0606c9a5ff94fae7fbae8d590a1fbf6a4
pierreEnoc <pierre_enoc@yahoo.fr>
2020-05-31 21:03:27 -0300

    enabling cors and connecting login with REST API

--- src/views/login.js
+++ src/views/login.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../components/card'
 import FormGroup from  '../components/form-group'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 class Login extends React.Component {
 
@@ -10,9 +11,16 @@ class Login extends React.Component {
         senha: ''
     }
 
-    entrar = () =>{
-        console.log('Email: ', this.state.email)
-        console.log('senha:', this.state.senha)
+    entrar = () => {
+        axios
+        .post('http://localhost:8080/api/usuarios/autenticar', {
+            email: this.state.email,
+            senha: this.state.senha
+        }).then( response => {
+            console.log(response)
+        }).catch( erro => {
+            console.log(erro.response)
+        })
     }
 
     prepareCadastrar = () => {
