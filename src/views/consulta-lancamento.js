e4778a1a12e2f7ca2caa4a8df48fd6e67b14b025
pierreEnoc <pierre_enoc@yahoo.fr>
2020-06-01 20:02:24 -0300

    creat component select menu combo box

--- src/views/consulta-lancamento.js
+++ src/views/consulta-lancamento.js
@@ -2,10 +2,28 @@ import React from 'react'
 import {withRouter} from 'react-router-dom'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
+import SelectMenu from '../components/selectMenu'
 
 class ConsultaLancamentos extends React.Component {
 
     render(){
+        const lista  = [
+            { label: 'selecione...', value: '' },
+            { label: 'Janeiro', value: 1 },
+            { label: 'Fevereiro', value: 2 },
+            { label: 'Marcço', value: 3 },
+            { label: 'Abril', value: 4 },
+            { label: 'Maio', value: 5 },
+            { label: 'Junho', value: 6 },
+            { label: 'Julho', value: 7 },
+            { label: 'Agosto', value: 8 },
+            { label: 'Septembro', value: 9 },
+            { label: 'Outobro', value: 10 },
+            { label: 'Novembro', value: 11},
+            { label: 'Dezembro', value: 12 }
+            
+        ]
+
         return(
             <Card title= "Consulta Lancamentos">
                 <div className="row">
@@ -19,6 +37,7 @@ class ConsultaLancamentos extends React.Component {
                             placeholder="Digite o Ano" />
                             </FormGroup>
                             <FormGroup htmlFor = "inputAno" label ="Mês: ">
+                                <SelectMenu className ="form-control" lista={lista} />
                         
                             </FormGroup>
 
