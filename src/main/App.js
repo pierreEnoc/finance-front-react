33cd332c0b2757bdd5cab6fc30f4cbdef95892e9
pierreEnoc <pierre_enoc@yahoo.fr>
2020-06-03 16:29:03 -0300

    add bibliotech primereact

--- src/main/App.js
+++ src/main/App.js
@@ -7,12 +7,19 @@ import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
 import 'toastr/build/toastr.css'
 
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import {Button} from 'primereact/button'
+
 class App extends React.Component {
   
   render(){
   return (
   <>
     <Navbar />
+    <Button  label="Click" icon="pi pi-check" iconPos="right" />
     <div className="container">
        <Rotas />
     </div>
