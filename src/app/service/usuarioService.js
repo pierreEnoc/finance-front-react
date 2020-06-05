b5f62eac8cc35659bb3932eba03d885c09b12f23
pierreEnoc <pierre_enoc@yahoo.fr>
2020-06-05 15:25:52 -0300

    refacto user form

--- src/app/service/usuarioService.js
+++ src/app/service/usuarioService.js
@@ -1,4 +1,6 @@
 import ApiService from '../apiservise'
+import ErroValidacao from '../exception/erroValidacao'
+import erroValidacao from '../exception/erroValidacao';
 
 class UsuarioService extends ApiService {
 
@@ -16,6 +18,31 @@ class UsuarioService extends ApiService {
 
     salvar(usuario){
         return this.post('/', usuario);
+    }
+
+    validar(usuario){
+
+        const erros = []
+        if(!usuario.nome){
+            erros.push('O campo Nome é obrigatório. ')
+        }
+
+        if(!usuario.email){
+            erros.push('O campo Email é obrigatório. ')
+        }else if ( !usuario.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
+            erros.push('Informe um Email valido.')
+        }
+
+        if(!usuario.senha || !usuario.senhaRepeticao){
+            erros.push('Digite a senha 2x.')
+        }else if(usuario.senha !== usuario.senhaRepeticao){
+            erros.push('As senha não batem.')
+        }
+
+        if(erros && erros.length > 0){
+            throw new erroValidacao(erros);
+        }
+        //return erros;
     }
 
 }
--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -21,42 +21,18 @@ class CadastroUsuario extends React.Component {
         this.service = new UsuarioService();
     }
 
-    validar(){
-        const msgs = []
-            if(!this.state.nome){
-                msgs.push('O campo Nome é obrigatório. ')
-            }
 
-            if(!this.state.email){
-                msgs.push('O campo Email é obrigatório. ')
-            }else if ( !this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
-                msgs.push('Informe um Email valido.')
-            }
+ cadastrar = () => {
+     
+     const {nome, email, senha, senhaRepeticao} = this.state
+     const usuario = {nome,email,senha, senhaRepeticao }
 
-            if(!this.state.senha || !this.state.senhaRepeticao){
-                msgs.push('Digite a senha 2x.')
-            }else if(this.state.senha !== this.state.senhaRepeticao){
-                msgs.push('As senha não batem.')
-            }
-
-
-        return msgs;
-    }
-
- cadastrar = () =>{
-     const msgs = this.validar();
-
-     if(msgs && msgs.length > 0){
-         msgs.forEach((msgs, index) => {
-             mensagemErro(msgs)
-         });
-         return false;
-     }
-
-     const usuario = {
-         nome: this.state.nome,
-         email: this.state.email,
-         senha: this.state.senha
+     try{
+        this.service.validar(usuario);
+     }catch(erro){
+        const msgs = erro.mensagens;
+        msgs.forEach(element => mensagemErro());
+        return false
      }
 
      this.service.salvar(usuario)
